9bc53c79a33f414478e8c778e67ce24daf904fdf
Anil Anar <anilanar@hotmail.com>
2017-04-07 23:37:20 +0200

    prevent triggering build for the same dir multiple times

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const spawn = require('cross-spawn');
 
-function NpmBuild() {}
+function NpmBuild() {
+    this.builtRoots = {};
+}
 
-NpmBuild.prototype.apply = (compiler) => {
+NpmBuild.prototype.apply = function (compiler) {
     compiler.plugin("after-resolvers", compiler => {
         compiler.resolvers.normal.plugin('directory', (request, cb) => {
             const descRoot = request.descriptionFileRoot;
             const desc = request.descriptionFileData;
+
+            if (this.builtRoots[descRoot] === true) {
+                cb(undefined, request);
+                return;
+            }
 
             const hasBuildTask = desc !== undefined
                 && desc.scripts !== undefined
@@ -17,6 +24,7 @@ NpmBuild.prototype.apply = (compiler) => {
                 return;
             }
 
+            this.builtRoots[descRoot] = true;
             runBuild(descRoot, function (err) {
                 if (!err) {
                     cb(undefined, request);

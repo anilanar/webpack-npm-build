bb565e8c4c3848066ca66078ff3f76f564e3033c
Anil Anar <anilanar@hotmail.com>
2017-04-08 14:57:27 +0200

    fix algorithm to find pkgs to build, add simple logging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const spawn = require('cross-spawn');
+const path = require('path');
+
+const ERROR = -1;
+const NOT_BUILT = 0;
+const IN_PROGRESS = 1;
+const BUILT = 2;
+
+function BuildInfo(type, payload) {
+    return {type, payload};
+}
+
+function hasType(buildInfo, type) {
+    return buildInfo.type === type;
+};
+
+function getPayload(buildInfo) {
+    return buildInfo.payload;
+}
 
 function NpmBuild() {
-    this.builtRoots = {};
-    this.builtRoots[process.cwd()] = true;
+    this.buildInfo = {};
+    this.root = process.cwd();
 }
 
 NpmBuild.prototype.apply = function (compiler) {
@@ -11,7 +29,8 @@ NpmBuild.prototype.apply = function (compiler) {
             const descRoot = request.descriptionFileRoot;
             const desc = request.descriptionFileData;
 
-            if (this.builtRoots[descRoot] === true) {
+            // ignore root
+            if (descRoot === this.root) {
                 cb(undefined, request);
                 return;
             }
@@ -25,34 +44,95 @@ NpmBuild.prototype.apply = function (compiler) {
                 return;
             }
 
-            this.builtRoots[descRoot] = true;
-            runBuild(descRoot, function (err) {
-                if (!err) {
+            const main = desc && desc.main;
+
+            if (main === undefined) {
+                console.error(Message(
+                    'To be able to run npm build, pkg.main must be defined.'
+                ));
+                cb(undefined, undefined);
+                return;
+            }
+
+            if (request.path !== path.join(descRoot, main)) {
+                // ignore current resolve request, but start building
+                this.build(descRoot);
+                cb(undefined, undefined);
+                return;
+            }
+
+            const prevBuildInfo = this.buildInfo[descRoot];
+
+            if (prevBuildInfo === undefined) {
+                this.build(descRoot).then(buildInfo => {
                     cb(undefined, request);
-                } else {
-                    cb('Could not build submodule');
-                }
-            });
+                }).catch(err => {
+                    console.error(Message(getPayload(err)));
+                    cb(undefined, undefined);
+                });
+            } else if(hasType(prevBuildInfo, ERROR)) {
+                console.error(Message(getPayload(prevBuildInfo)));
+                cb(undefined, undefined);
+            } else if(hasType(prevBuildInfo, IN_PROGRESS)) {
+                getPayload(prevBuildInfo).then(() => {
+                    cb(undefined, request);
+                }).catch((err) => {
+                    console.error(Message(getPayload(err)));
+                    cb(undefined, undefined);
+                });
+            } else {
+                cb(undefined, request);
+            }
         });
     });
 }
 
-function runBuild(descRoot, callback) {
+NpmBuild.prototype.build = function (root) {
+    console.log(Message(`Build in progress: ${root}`));
+
+    const progress = new Promise((res, rej) => {
+        runBuild(root, err => {
+            if (!err) {
+                console.log(Message(`Built: ${root}`));
+                const buildInfo = BuildInfo(BUILT);
+                this.buildInfo[root] = buildInfo;
+                res(buildInfo);
+            } else {
+                const buildInfo = BuildInfo(
+                    ERROR,
+                    Message(`Could not build submodule: ${root}`)
+                );
+                this.buildInfo[root] = buildInfo;
+                rej(buildInfo);
+            }
+        });
+    });
+
+    this.buildInfo[root] = BuildInfo(IN_PROGRESS, progress);
+
+    return progress;
+};
+
+function runBuild(root, callback) {
     const npm = spawn('npm', [
         'run',
         'build'
     ], {
         stdio: ['ignore', 'pipe', 'inherit'],
-        cwd: descRoot,
+        cwd: root,
     });
 
     npm.on('close', code => {
         if (code !== 0) {
             callback(code);
         } else {
-            callback();
+            callback(0);
         }
     });
+}
+
+function Message(str) {
+    return `[webpack-npm-build] ${str}`;
 }
 
 module.exports = NpmBuild;
